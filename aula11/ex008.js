905213f3f366a54a00b6b256138cd50d7d364abe
Higor Cavalcante <92644248+HigorCavalcante@users.noreply.github.com>
2021-10-27 16:52:12 -0200

    Initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,4 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel}km/H`)
+if (vel > 60) {console.log(`A velocidade do seu carro utrapassou o limte permitido. Mutadado!`) }
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var páis = 'Brasil'
+console.log(`Vivendo em  ${páis}`)
+if (páis != brasil) {
+    console.log('Você é  Estageiro')
+} else {
+    console.log('Você é Brasilheiro')
+}
